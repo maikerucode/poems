080ffb4edf8f1e397745da466e0f5a34cc91547e
maikerucode <65951580+maikerucode@users.noreply.github.com>
2024-10-06 14:16:45 +0800

    forms initialized, File upload configured, making use of .txt filetype

--- resources/views/layouts/app_exam.blade.php
+++ resources/views/layouts/app_exam.blade.php
@@ -11,6 +11,7 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
         <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
+        <link href="https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.css" rel="stylesheet" />
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
@@ -23,5 +24,6 @@
                 @yield('content')
             </main>
         </div>
+        <script src="https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.js"></script>
     </body>
 </html>
